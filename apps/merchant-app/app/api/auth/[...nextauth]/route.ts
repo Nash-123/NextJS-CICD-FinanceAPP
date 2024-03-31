84326f3ce596d24ac40825c0b01d3234be0af212
Nishan Acharya <mamabapuni@gmail.com>
2024-03-31 21:03:49 +0530

    New changes

--- apps/merchant-app/app/api/auth/[...nextauth]/route.ts
+++ apps/merchant-app/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import { authOptions } from "../../../../lib/auth"
+import { NextApiRequest } from "../../../../../../node_modules/next/dist/shared/lib/utils"
 
 //@ts-ignore
 const handler = NextAuth(authOptions)
